e5b2b706df70ff7cce373d44ec7d8d60fd88db42
KKyeonguk <112309361+KKyeonguk@users.noreply.github.com>
2022-09-01 14:25:48 +0900

    first commit

---
+++ test_python.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+#reportMissingModuleSource 문제 해결 :  cmd -> pip install requests // 여러 개의 파이썬 버전이 존재하여 본인이 사용하는 버전이 아닌 다른 버전의 파이썬에 대해 requests 라이브러리가 설치된 것이므로, vscode 윈도우 맨 하단 상태줄에 버전 정보를 클릭하여 다른 버전으로 하면 됨!
+
+
+data = pd.read_csv('')
+
+
